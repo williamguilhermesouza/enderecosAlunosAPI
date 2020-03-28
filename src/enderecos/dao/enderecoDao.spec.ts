95a4dcbbd737a0fe554087a5b4a1715aa3255da5
william <williamguilhermesouza@gmail.com>
2020-03-28 13:27:45 -0300

    working on enderecoDao test

--- src/enderecos/dao/enderecoDao.spec.ts
+++ src/enderecos/dao/enderecoDao.spec.ts
@@ -1,1 +1,25 @@
 import { Test } from '@nestjs/testing';
+import { enderecoDaoModel } from './enderecomodel.dao';
+import { enderecoDao } from './enderecoimplementation.dao';
+
+describe('enderecoDAO', () => {
+    let EnderecoDao: enderecoDao;
+    let EnderecoDaoModel: enderecoDaoModel;
+    
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers:  [enderecoDaoModel, enderecoDao]
+        }).compile();
+
+        EnderecoDaoModel = module.get<enderecoDaoModel>(enderecoDaoModel);
+        EnderecoDao = module.get<enderecoDao>(enderecoDao);
+
+    });
+
+    describe('find', async () => {
+        it('should return all enderecos', () => {
+            let resp = EnderecoDao.find();
+            expect(resp).toBeCalled(); 
+        });
+    });
+});
