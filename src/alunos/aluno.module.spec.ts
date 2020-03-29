ab2a8e040a7f9a2709ce7ffd77977ac60d5ce370
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-03-28 22:46:08 -0300

    working on enderecoDao tests

--- src/alunos/aluno.module.spec.ts
+++ src/alunos/aluno.module.spec.ts
@@ -1,6 +1,10 @@
 import { Test } from '@nestjs/testing';
 import { alunoService } from './aluno.service';
 import { alunoController } from './aluno.controller';
+import { alunoDao } from './dao/alunoimplementation.dao';
+import { Aluno } from './aluno.entity';
+
+jest.mock('./dao/alunoimplementation.dao');
 
 describe('alunoController', () => {
     let AlunoController: alunoController;
@@ -19,6 +23,19 @@ describe('alunoController', () => {
 
     describe('findAllAlunos', () => {
         it('should return all alunos', async () => {
+            const result: Aluno[] = [{
+                id: 1,
+                nome: 'william',
+                data_nascimento: new Date('10-06-26 02:31:29,573'),
+                cpf: '89508434040',
+                nota: 10,
+            },];
+            
+            jest.spyOn(AlunoService, 'create').mockImplementation(() => { 
+                return new Promise((resolve, reject) => {result});
+            });
+
+            return AlunoController.findAllAlunos().then(data => expect(data).toBe(result));
             
         })
     })
--- src/enderecos/dao/enderecoDao.spec.ts
+++ src/enderecos/dao/enderecoDao.spec.ts
@@ -1,14 +1,64 @@
 import { Test } from '@nestjs/testing';
 import { enderecoDaoModel } from './enderecomodel.dao';
 import { enderecoDao } from './enderecoimplementation.dao';
+import { Endereco } from '../endereco.entity';
+
+jest.mock('./enderecomodel.dao');
+
+class EnderecoDaoModelMock {
+    __save(endereco: Endereco) {
+        return jest.fn((endereco) => endereco);
+    }
+    __find() {
+        const result: Endereco[] = [{
+            id: 1,
+            rua: 'riodades',
+            bairro: 'fonseca',
+            numero: '143',
+            complemento: 'apt 202',
+            aluno_id: 1
+        },];
+
+        return result;
+    }
+    __filter(bairro) {
+        const result: Endereco[] = [{
+            id: 1,
+            rua: 'riodades',
+            bairro: bairro,
+            numero: '143',
+            complemento: 'apt 202',
+            aluno_id: 1
+        },];
+
+        return result;
+    }
+    __queryAlunoEndereco(id: number) {
+        return {'total': 1,
+        'enderecos': {
+            rua: 'riodades',
+            numero: '143',
+            complemento: 'apt 202',
+            bairro: 'fonseca',
+        },
+    };
+;
+    }
+}
 
 describe('enderecoDAO', () => {
     let EnderecoDao: enderecoDao;
     let EnderecoDaoModel: enderecoDaoModel;
+
     
     beforeEach(async () => {
+        const enderecoDaoModelProvider = {
+            provide: enderecoDaoModel,
+            useClass: EnderecoDaoModelMock,
+        }
+
         const module = await Test.createTestingModule({
-            providers:  [enderecoDaoModel, enderecoDao]
+            providers:  [enderecoDaoModelProvider, enderecoDao]
         }).compile();
 
         EnderecoDaoModel = module.get<enderecoDaoModel>(enderecoDaoModel);
@@ -16,10 +66,40 @@ describe('enderecoDAO', () => {
 
     });
 
-    describe('find', async () => {
-        it('should return all enderecos', () => {
-            let resp = EnderecoDao.find();
-            expect(resp).toBeCalled(); 
+    beforeAll(() => {
+        const enderecoArray: Endereco[] = [{
+            id: 1,
+            rua: 'riodades',
+            bairro: 'fonseca',
+            numero: '143',
+            complemento: 'apt 202',
+            aluno_id: 1
+        },];
+
+        const endereco = {
+            id: 1,
+            rua: 'riodades',
+            bairro: 'fonseca',
+            numero: '143',
+            complemento: 'apt 202',
+            aluno_id: 1
+        };
+
+        const enderecoQuery = {
+            'total': 1,
+            'enderecos': {
+                rua: 'riodades',
+                numero: '143',
+                complemento: 'apt 202',
+                bairro: 'fonseca',
+            },
+        };
+    });
+
+    describe('find without params', () => {
+        it('should return all enderecos', async () => {
+            const result = await EnderecoDao.find();
+            expect(this.enderecoArray).toEqual(result);
         });
     });
 });
